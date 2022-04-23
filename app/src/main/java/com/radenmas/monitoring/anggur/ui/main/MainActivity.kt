94d65fbd5f021cbc212493338b8cd0419758370b
RadenMas6699 <radenmas.dev@gmail.com>
2022-04-23 09:37:39 +0800

    Add Func

--- app/src/main/java/com/radenmas/monitoring/anggur/ui/main/MainActivity.kt
+++ app/src/main/java/com/radenmas/monitoring/anggur/ui/main/MainActivity.kt
@@ -5,19 +5,25 @@
 
 package com.radenmas.monitoring.anggur.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.radenmas.monitoring.anggur.R
 import com.radenmas.monitoring.anggur.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var b: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -27,9 +33,31 @@ class MainActivity : AppCompatActivity() {
         val v = b.root
         setContentView(v)
 
-        val navController = findNavController(R.id.fragmentContainerView)
+        val dt = Date()
+        when (dt.hours) {
+            in 1..10 -> {
+                b.tvTime.text = "Selamat Pagi"
+                b.imgTime.setImageResource(R.drawable.ic_siang)
+            }
+            in 11..14 -> {
+                b.tvTime.text = "Selamat Siang"
+                b.imgTime.setImageResource(R.drawable.ic_siang)
+            }
+            in 15..18 -> {
+                b.tvTime.text = "Selamat Sore"
+                b.imgTime.setImageResource(R.drawable.ic_siang)
+            }
+            in 19..24 -> {
+                b.tvTime.text = "Selamat Malam"
+                b.imgTime.setImageResource(R.drawable.ic_malam)
+            }
+        }
 
+        val navController = findNavController(R.id.fragment)
         b.navBottom.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            b.tvTitle.text = destination.label
+        }
 
     }
 }
