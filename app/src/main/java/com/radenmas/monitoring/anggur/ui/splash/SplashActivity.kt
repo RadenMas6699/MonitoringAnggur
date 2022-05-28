864b9f733147343b8b3b756c0a5a8bd67e5bd785
RadenMas6699 <radenmas.dev@gmail.com>
2022-05-28 23:42:15 +0800

    Add program app version on SplashActivity.kt

--- app/src/main/java/com/radenmas/monitoring/anggur/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/radenmas/monitoring/anggur/ui/splash/SplashActivity.kt
@@ -12,19 +12,26 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.radenmas.monitoring.anggur.ui.main.MainActivity
 import com.radenmas.monitoring.anggur.R
+import com.radenmas.monitoring.anggur.databinding.ActivitySplashBinding
+import com.radenmas.monitoring.anggur.ui.main.MainActivity
 
 /**
  * Created by RadenMas on 17/03/2022.
  */
 class SplashActivity : AppCompatActivity() {
+    private lateinit var b: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
-        setContentView(R.layout.activity_splash)
+        b = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        val version = packageManager.getPackageInfo(packageName, 0).versionName
+        b.tvAppVersion.text = resources.getString(R.string.app_version, version)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
